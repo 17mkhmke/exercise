31a9ec534ff7d96570b3db1ef52b0a91db317dc3
KingOfCoders <17mkhuselimkeyiya@gmail.com>
2022-12-09 16:52:45 +0200

    IStandChance

--- js/admin.js
+++ js/admin.js
@@ -1,131 +1,155 @@
-//CRUD
-// let products = {
-//     data:[{
-//         id: 1,
-//         productName: "Budwiser",
-//         image : "https://i.postimg.cc/JhCpLBwq/Bud-Special.png",
-//         price: "R200",
-//         category:"beer",
-//         quantity: "24 x 330ml"
-//     },
-//     {
-//                 id: 2,
-//                 productName: "savanna",
-//                 image : "https://i.postimg.cc/9z377ZYg/Savanna-Pack.jpg",
-//                 price: "R200",
-//                 category: "cider",
-//                 quantity: "24 x 330ml"
-        
-//             },
-//             {
-//                 id: 3,
-//                 productName: "Russian Bear",
-//                 image : "https://i.postimg.cc/P5TMVn13/Russian-Bear.png",
-//                 price: "R154",
-//                 category: "vodka",
-//                 quantity: "1 litre"
-        
-//             },
-//             {
-//                 id: 4,
-//                 productName: "4th-street",
-//                 image : "https://i.postimg.cc/pLcnfwCn/4th-Street-Wine.png",
-//                 price: "R130",
-//                 category: "wine",
-//                 quantity: "5 litres"
-        
-//             },
-//             {
-//                 id: 5,
-//                 productName: "firstwatch",
-//                 image : "https://i.postimg.cc/TPWhrphZ/first-Watch.jpg",
-//                 price: "R170",
-//                 category: "whisky",
-//                 quantity: "750ml"
-        
-//             },
-//             {
-//                 id: 6,
-//                 productName: "J.C. Le Roux Domaine Premium",
-//                 image : "https://i.postimg.cc/4dsZmdcv/LeRoux.png",
-//                 price: "R185",
-//                 category: "champaign",
-//                 quantity: "750ml"
-        
-//             },
-//             {
-//                 id: 7,
-//                 productName: "Flying Fish",
-//                 image : "https://i.postimg.cc/WbCxK35N/flying-Fish2-0.webp",
-//                 price: "R100",
-//                 category: "beer",
-//                 quantity: "500ml X 6"
-        
-//             },
-//             {
-//                 id: 8,
-//                 productName: "Bernini Blush",
-//                 image : "https://i.postimg.cc/mrT69Fdr/Bernini-Blush.png",
-//                 price: "R270",
-//                 category: "cider",
-//                 quantity: "440ml x 6"
-        
-//             },
-//             {
-//                 id: 9,
-//                 productName: "Old Buck",
-//                 image : "https://i.postimg.cc/JzQCS8vG/oldBuck.png",
-//                 price: "R130",
-//                 category: "gin",
-//                 quantity: "750ml"
-        
-//             },
-//             {
-//                 id: 10,
-//                 productName: "Hennesy",
-//                 image : "https://i.postimg.cc/D0KPs96K/Hennesy2-1.png",
-//                 price: "R420",
-//                 category: "brandy",
-//                 quantity: "750ml"
-        
-//             },
-//             {
-//                 id: 11,
-//                 productName: "Gordon's Gin",
-//                 image : "https://i.postimg.cc/50BBSP6d/Gordons.png",
-//                 price: "R180",
-//                 category: "gin",
-//                 quantity: "750ml"
+ var products = {
+    data:[
+        {
+            id: 1,
+            productName: "Budwiser",
+            image : "https://i.postimg.cc/JhCpLBwq/Bud-Special.png",
+            price: 200,
+            category:"beer",
+            quantity: "24 x 330ml"
+        },
+        {
+            id: 2,
+            productName: "savanna",
+            image : "https://i.postimg.cc/9z377ZYg/Savanna-Pack.jpg",
+            price: 200,
+            category: "cider",
+            quantity: "24 x 330ml"
+        },
+        {
+            id: 3,
+            productName: "Russian Bear",
+            image : "https://i.postimg.cc/P5TMVn13/Russian-Bear.png",
+            price: 154,
+            category: "vodka",
+            quantity: "1 litre"
+        },
+        {
+            id: 4,
+            productName: "4th-street",
+            image : "https://i.postimg.cc/pLcnfwCn/4th-Street-Wine.png",
+            price: 130,
+            category: "wine",
+            quantity: "5 litres"
+        },
+        {
+            id: 5,
+            productName: "firstwatch",
+            image : "https://i.postimg.cc/TPWhrphZ/first-Watch.jpg",
+            price: 170,
+            category: "whisky",
+            quantity: "750ml"
+        },
+        {
+            id: 6,
+            productName: "J.C. Le Roux Domaine Premium",
+            image : "https://i.postimg.cc/4dsZmdcv/LeRoux.png",
+            price: 185,
+            category: "champaign",
+            quantity: "750ml"
+        },
+        {
+            id: 7,
+            productName: "Flying Fish",
+            image : "https://i.postimg.cc/WbCxK35N/flying-Fish2-0.webp",
+            price: 100,
+            category: "beer",
+            quantity: "500ml X 6"
+        },
+        {
+            id: 8,
+            productName: "Bernini Blush",
+            image : "https://i.postimg.cc/mrT69Fdr/Bernini-Blush.png",
+            price: 270,
+            category: "cider",
+            quantity: "440ml x 6"
+        },
+        {
+            id: 9,
+            productName: "Old Buck",
+            image : "https://i.postimg.cc/JzQCS8vG/oldBuck.png",
+            price: 130,
+            category: "gin",
+            quantity: "750ml"
+        },
+        {
+            id: 10,
+            productName: "Hennesy",
+            image : "https://i.postimg.cc/D0KPs96K/Hennesy2-1.png",
+            price: 420,
+            category: "brandy",
+            quantity: "750ml"
+        },
+        {
+            id: 11,
+            productName: "Gordon's Gin",
+            image : "https://i.postimg.cc/50BBSP6d/Gordons.png",
+            price: 180,
+            category: "gin",
+            quantity: "750ml"
+        },
+        {
+            id: 12,
+            productName: "Rain Dance",
+            image : "https://i.postimg.cc/8zxLQ8vg/raindance.jpg",
+            price: 110,
+            category: "wine",
+            quantity : "5 litres"
+        },
+    ],
+};
 
-        
-//             },
-//             {
-//                 id: 12,
-//                 productName: "Rain Dance",
-//                 image : "https://i.postimg.cc/8zxLQ8vg/raindance.jpg",
-//                 price: "R110",
-//                 category: "wine",
-//                 quantity : "5 litres"
-        
-//             },
-// ],
-// };
-// Global variables
-function submit(){
-    var dataEntered = retrieveData();
-}
-function retrieveData(){
-    var id = document.getElementById("id").value;
-    var productName = document.getElementById("productName").value;
-    var price = document.getElementById("price").value;
-    var category = document.getElementById("category").value;
-    var quantity = document.getElementById("quantity").value;
-    var image = document.getElementById("image").value;
-   var products =[id, productName, price, category, quantity, image];
-   return products;
-}
-let add= document.querySelector('#btn')
+
+console.table(products.data);
+document.write
+let add = document.querySelector('#btn')
 add.addEventListener('click',(e)=>{
     e.preventDefault();
     alert("Item added to storage");
 })
+// function onFormSubmit(){
+//     var formData = readFormData();
+//     insertNewRecord(formData);
+// }
+// function readFormData(){
+//     var formData ={};
+//     formData["id"] = document.getElementById("id").value;
+//     formData["ProductName"] = document.getElementById("productName").value;
+//     formData["price"] = document.getElementById("price").value;
+//     formData["category"] = document.getElementById("category").value;
+//     formData["quantity"] = document.getElementById("quantity").value;
+//     formData["image"] = document.getElementById("image").value;
+//     return formData;
+// }
+
+// function insertNewRecord(data) {
+//     var table = document.getElementById("productList").getElementsByTagName('tbody')[0];
+//     var newRow = table.insertRow(table.length);
+//     cell1 = newRow.insertCell(0);
+//     cell1.innerHTML = data.id;
+//     cell2 = newRow.insertCell(1);
+//     cell2.innerHTML = data.productName;
+//     cell3 = newRow.insertCell(2);
+//     cell3.innerHTML = data.price;
+//     cell4 = newRow.insertCell(3);
+//     cell4.innerHTML = data.category;
+//     cell5 = newRow.insertCell(4);
+//     cell5.innerHTML = data.quantity;
+//     cell6 = newRow.insertCell(5);
+//     cell6.innerHTML = `<a>Edit</a><a>Delete</a>`;
+// }
+
+// function submit(){
+//     var dataEntered = retrieveData();
+// }
+// function retrieveData(){
+//     var id = document.getElementById("id").value;
+//     var productName = document.getElementById("productName").value;
+//     var price = document.getElementById("price").value;
+//     var category = document.getElementById("category").value;
+//     var quantity = document.getElementById("quantity").value;
+//     var image = document.getElementById("image").value;
+//    var products =[id, productName, price, category, quantity, image];
+//    return products;
+// }
+
--- js/code.js
+++ js/code.js
@@ -1,132 +1,120 @@
-let products = {
-    data:[{
-        id: 1,
-        productName: "Budwiser",
-        image : "https://i.postimg.cc/JhCpLBwq/Bud-Special.png",
-        price: 200,
-        category:"beer",
-        quantity: "24 x 330ml",
-        button :"Add"
-    },
-    {
-                id: 2,
-                productName: "savanna",
-                image : "https://i.postimg.cc/9z377ZYg/Savanna-Pack.jpg",
-                price: 200,
-                category: "cider",
-                quantity: "24 x 330ml",
-                button :"Add"
-        
-            },
-            {
-                id: 3,
-                productName: "Russian Bear",
-                image : "https://i.postimg.cc/P5TMVn13/Russian-Bear.png",
-                price: 154,
-                category: "vodka",
-                quantity: "1 litre",
-                button :"Add"
-        
-            },
-            {
-                id: 4,
-                productName: "4th-street",
-                image : "https://i.postimg.cc/pLcnfwCn/4th-Street-Wine.png",
-                price: 130,
-                category: "wine",
-                quantity: "5 litres",
-                button :"Add"
-        
-            },
-            {
-                id: 5,
-                productName: "firstwatch",
-                image : "https://i.postimg.cc/TPWhrphZ/first-Watch.jpg",
-                price: 170,
-                category: "whisky",
-                quantity: "750ml",
-                button :"Add"
-        
-            },
-            {
-                id: 6,
-                productName: "J.C. Le Roux Domaine Premium",
-                image : "https://i.postimg.cc/4dsZmdcv/LeRoux.png",
-                price: 185,
-                category: "champaign",
-                quantity: "750ml",
-                button :"Add"
-        
-            },
-            {
-                id: 7,
-                productName: "Flying Fish",
-                image : "https://i.postimg.cc/WbCxK35N/flying-Fish2-0.webp",
-                price: 100,
-                category: "beer",
-                quantity: "500ml X 6",
-                button :"Add"
-        
-            },
-            {
-                id: 8,
-                productName: "Bernini Blush",
-                image : "https://i.postimg.cc/mrT69Fdr/Bernini-Blush.png",
-                price: 270,
-                category: "cider",
-                quantity: "440ml x 6",
-                button :"Add",
-                button :"Add"
-        
-            },
-            {
-                id: 9,
-                productName: "Old Buck",
-                image : "https://i.postimg.cc/JzQCS8vG/oldBuck.png",
-                price: 130,
-                category: "gin",
-                quantity: "750ml",
-                button :"Add"
-        
-            },
-            {
-                id: 10,
-                productName: "Hennesy",
-                image : "https://i.postimg.cc/D0KPs96K/Hennesy2-1.png",
-                price: 420,
-                category: "brandy",
-                quantity: "750ml"
-        
-            },
-            {
-                id: 11,
-                productName: "Gordon's Gin",
-                image : "https://i.postimg.cc/50BBSP6d/Gordons.png",
-                price: 180,
-                category: "gin",
-                quantity: "750ml",
-                button :"Add"
-
-        
-            },
-            {
-                id: 12,
-                productName: "Rain Dance",
-                image : "https://i.postimg.cc/8zxLQ8vg/raindance.jpg",
-                price: 110,
-                category: "wine",
-                quantity : "5 litres",
-                button :"Add"
-
-                
-        
-            },
-],
+var products = {
+    data:[
+        {
+            id: 1,
+            productName: "Budwiser",
+            image : "https://i.postimg.cc/JhCpLBwq/Bud-Special.png",
+            price: 200,
+            category:"beer",
+            quantity: "24 x 330ml ",
+            button :"Add to cart"
+        },
+        {
+            id: 2,
+            productName: "savanna",
+            image : "https://i.postimg.cc/9z377ZYg/Savanna-Pack.jpg",
+            price: 200,
+            category: "cider",
+            quantity: "24 x 330ml ",
+            button :"Add to cart"
+        },
+        {
+            id: 3,
+            productName: "Russian Bear",
+            image : "https://i.postimg.cc/P5TMVn13/Russian-Bear.png",
+            price: 154,
+            category: "vodka",
+            quantity: "1 litre ",
+            button :"Add to cart"
+        },
+        {
+            id: 4,
+            productName: "4th-street",
+            image : "https://i.postimg.cc/pLcnfwCn/4th-Street-Wine.png",
+            price: 130,
+            category: "wine",
+            quantity: "5 litres ",
+            button :"Add to cart"
+        },
+        {
+            id: 5,
+            productName: "firstwatch",
+            image : "https://i.postimg.cc/TPWhrphZ/first-Watch.jpg",
+            price: 170,
+            category: "whisky",
+            quantity: "750ml ",
+            button :"Add to cart"
+        },
+        {
+            id: 6,
+            productName: "J.C. Le Roux Domaine Premium",
+            image : "https://i.postimg.cc/4dsZmdcv/LeRoux.png",
+            price: 185,
+            category: "champaign",
+            quantity: "750ml ",
+            button :"Add to cart"
+        },
+        {
+            id: 7,
+            productName: "Flying Fish",
+            image : "https://i.postimg.cc/WbCxK35N/flying-Fish2-0.webp",
+            price: 100,
+            category: "beer",
+            quantity: "500ml X 6 ",
+            button :"Add to cart"
+        },
+        {
+            id: 8,
+            productName: "Bernini Blush",
+            image : "https://i.postimg.cc/mrT69Fdr/Bernini-Blush.png",
+            price: 270,
+            category: "cider",
+            quantity: "440ml x 6 ",
+            button :"Add to cart"
+        },
+        {
+            id: 9,
+            productName: "Old Buck",
+            image : "https://i.postimg.cc/JzQCS8vG/oldBuck.png",
+            price: 130,
+            category: "gin",
+            quantity: "750ml ",
+            button :"Add to cart"
+        },
+        {
+            id: 10,
+            productName: "Hennesy",
+            image : "https://i.postimg.cc/D0KPs96K/Hennesy2-1.png",
+            price: 420,
+            category: "brandy",
+            quantity: "750ml ",
+            button: "Add to cart"
+        },
+        {
+            id: 11,
+            productName: "Gordon's Gin",
+            image : "https://i.postimg.cc/50BBSP6d/Gordons.png",
+            price: 180,
+            category: "gin",
+            quantity: "750ml ",
+            button :"Add to cart"
+        },
+        {
+            id: 12,
+            productName: "Rain Dance",
+            image : "https://i.postimg.cc/8zxLQ8vg/raindance.jpg",
+            price: 110,
+            category: "wine",
+            quantity : "5 litres ",
+            button :"Add to cart"
+        },
+    ],
 };
 //Create cards
 for (let i of products.data){
     let card = document.createElement("div");
-    card.classList.add("card", "i.category", "hide");
+
+    card.classList.add("card", "i.category");
     let imgContainer = document.createElement("div");
     imgContainer.classList.add("image-container");
     let image = document.createElement("img");
@@ -137,7 +125,7 @@ for (let i of products.data){
     let container = document.createElement("div");
     container.classList.add("container");
     //prodName
-    let name = document.createElement("h5");
+    let name = document.createElement("h6");
     name.classList.add("product-name");
     name.innerText = i.productName.toUpperCase();
     container.appendChild(name);
@@ -157,13 +145,17 @@ for (let i of products.data){
     quants.classList.add("product-quantity");
     quants.innerText = i.quantity.toLowerCase();
     container.appendChild(quants);
-    card.appendChild(container)
+    
+    //buttons
+    let checkout = document.createElement("button");
+    checkout.classList.add("btnCheckout");
+    checkout.innerText = i.button.toLowerCase();
+    container.appendChild(checkout);
+    card.appendChild(container);
     document.getElementById("Products").appendChild(card);
+    
+}
 
-    //button
-
-
-};
 
 function filter(value){
 
@@ -176,140 +168,24 @@ function filter(value){
         }
     
 });
+
 //select all
 let elements = document.querySelectorAll(".card");
 elements.forEach((element)=>{
     if (value == "all"){
-        element.classList.remove("hide");
+        element.classList.remove();
     } else {
         if(element.classList.contains(value)){
-            element.classList.remove("hide")
+            element.classList.remove()
         }
         else{
-            element.classList.add("hide")
+            element.classList.add()
         }
     }
 })
 }
 //all
 window.onload =() =>{
-    filter('all');
+    filter("all");
 
 };
-
-// let products = JSON.parse(localStorage.getItem('products ')) ?
-// JSON.parse(localStorage.getItem('products ')) : [
-//     {
-//         id: 1,
-//         productName: "Budwiser",
-//         image : "",
-//         price: "R200",
-//         category:"beer",
-//         quantity: "24 x 330ml"
-
-//     },
-//     {
-//         id: 2,
-//         productName: "savanna",
-//         image : "",
-//         price: "R200",
-//         category: "cider",
-//         quantity: "24 x 330ml"
-
-//     },
-//     {
-//         id: 3,
-//         productName: "Russian Bear",
-//         image : "",
-//         price: "R154",
-//         category: "vodka",
-//         quantity: "1 litre"
-
-//     },
-//     {
-//         id: 4,
-//         productName: "4th-street",
-//         image : "",
-//         price: "R130",
-//         category: "wine",
-//         quantity: "3 litres"
-
-//     },
-//     {
-//         id: 5,
-//         productName: "firstwatch",
-//         image : "",
-//         price: "R200",
-//         category: "whisky",
-//         quantity: "750ml"
-
-//     },
-//     {
-//         id: 6,
-//         productName: "Martini Sparkling",
-//         image : "",
-//         price: "R185",
-//         category: "champaign"
-
-//     },
-//     {
-//         id: 7,
-//         productName: "Budwiser",
-//         image : "",
-//         price: "R200",
-//         category: "cider"
-
-//     },
-//     {
-//         id: 8,
-//         productName: "Budwiser",
-//         image : "",
-//         price: "R200",
-//         category: "cider"
-
-//     },
-//     {
-//         id: 9,
-//         productName: "Budwiser",
-//         image : "",
-//         price: "R200",
-//         category: "cider"
-
-//     },
-//     {
-//         id: 10,
-//         productName: "Budwiser",
-//         image : "",
-//         price: "R200",
-//         category: "cider"
-
-//     },
-//     {
-//         id: 11,
-//         productName: "Budwiser",
-//         image : "",
-//         price: "R200",
-//         category: "cider"
-
-//     },
-//     {
-//         id: 12,
-//         productName: "Budwiser",
-//         image : "",
-//         price: "R200",
-//         category: "cider"
-
-//     }
-// ];
-
-
-
-
-/*document.addEventListener('DOMContentLoaded',()=>{
-    let countDown = 1600019199
-    let flipdown = new flipdown(countDown)
-    .start()
-    .ifEnded(()=>{
-        document.querySelector(".flipdown").innerHTML = `<h2> Special has ended</h2>`
-    })
-})*/
